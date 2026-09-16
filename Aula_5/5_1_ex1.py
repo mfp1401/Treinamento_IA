cdc1f8ff96bdc70a6fc20640b629135a6cde7af2
mfp1401 <marcelo.froes@gec.inatel.br>
2026-09-16 09:25:03 -0300

    Exercícios de pipeline de machine learning

---
+++ Aula_5/5_1_ex1.py
@@ -0,0 +1,51 @@
+import pandas as pd
+from sklearn.datasets import load_wine
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import (accuracy_score,  confusion_matrix)
+from sklearn.dummy import DummyClassifier
+
+wine = load_wine()
+
+y = wine.target
+x = wine.data
+
+df = pd.DataFrame(wine.data, columns=wine.feature_names)
+df['especies'] = wine.target_names[wine.target]
+df.groupby('especies').mean().round(2)
+
+# Split dos dados em treino e teste
+x_train, x_test, y_train, y_test = train_test_split(
+    x, y, test_size=0.3, stratify=y, random_state=42)
+
+# KNN
+modelo = KNeighborsClassifier(n_neighbors=5)
+modelo.fit(x_train, y_train)
+
+# Prever o modelo
+y_pred = modelo.predict(x_test)
+
+# Comparar o modelo com o DummyClassifier
+
+base = DummyClassifier(strategy='most_frequent')
+base.fit(x_train, y_train)
+
+
+print(x.shape, y.shape)
+
+print(df.groupby('especies').mean().round(2))
+
+print(x_train.shape, x_test.shape)
+
+print(modelo)
+
+print(y_pred[:8])
+print(y_test[:8])
+
+# Avaliar o modelo
+
+print(accuracy_score(y_test, y_pred))
+print(confusion_matrix(y_test, y_pred))
+
+# comparando os modelos
+print(base.score(x_test, y_test))
